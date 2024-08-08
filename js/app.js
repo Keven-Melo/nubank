ecf0dfa3e76f635cd5db1c8eb75ecd03cf2e7339
Keven Melo <kevenmelodn@gmail.com>
2024-08-08 13:32:08 -0300

    Updated functionality in app.js

--- js/app.js
+++ js/app.js
@@ -55,3 +55,18 @@ dropdownMenu1.addEventListener('mouseleave', RemoveDropdown);
 dropdownMenu2.addEventListener('mouseleave', RemoveDropdown);
 dropdownMenu3.addEventListener('mouseleave', RemoveDropdown);
 
+// MENU MOBILE 
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId)
+    const nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            nav.classList.toggle('active-menu-mobile');
+            toggle.classList.toggle('active-bx')
+        })
+    }
+}
+
+showMenu('bx', 'menu-mobile')
